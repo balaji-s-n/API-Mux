e62e74b6da511175895fae8753094a6686f52963
unknown <balajisn1994@gmail.com>
2024-09-29 23:28:48 +0530

    without mux

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func roothandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "hi hello",
+	})
+}
 
 func main() {
 	fmt.Println("server is running")
+	log.Fatal(http.ListenAndServe("localhost:9000", http.HandlerFunc(roothandler)))
+
 }
